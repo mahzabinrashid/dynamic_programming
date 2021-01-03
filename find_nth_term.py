8e4a878ff88e62ae8c1056f148689d4c6215678b
Fariha <mahzabin741@gmail.com>
2021-01-04 03:57:18 +0600

    Update find_nth_term.py

--- find_nth_term.py
+++ find_nth_term.py
@@ -1,16 +1,15 @@
 # A bottom-up solution to find the n-th Fibonacci number
-def fib_bottom_up(n):
+def fib(n):
     # if we see the base cases, directly return the value 1
     if n == 1 or n == 2:
         return 1
     # making an array of length n
-    bottom_up = [None] * n
+    array = [None] * n
     # initializing the values for n = 1 and n = 2
-    bottom_up[0] = 1
-    bottom_up[1] = 1
+    array[0] = 1
+    array[1] = 1
     # filling up the array by using previous stored values
     for i in range(2, n):
-        bottom_up[i] = bottom_up[i - 1] + bottom_up[i - 2]
+        array[i] = array[i - 1] + array[i - 2]
     # returns the last item in the array
-    return bottom_up[n - 1]
-
+    return array[n - 1]
